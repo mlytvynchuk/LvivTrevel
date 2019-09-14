1d5a34ba78b224992ad419981e4702f703111f33
amserhii11 <amelin.serhii@gmail.com>
2019-09-14 17:21:10 +0300

    Add router

---
+++ src/components/Event/Event.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+class Event extends React.Component {
+  render() {
+    return (
+      <div className="event">
+        <Header />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Event;
