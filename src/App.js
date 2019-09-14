ec5bf75899fda9aa7887da9468310c9686c3a9bf
amserhii11 <amelin.serhii@gmail.com>
2019-09-14 15:43:07 +0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="page">
-      Hello React!
+      <Footer />
     </div>
   );
 }
