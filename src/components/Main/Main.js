af996a31e3bf08dbe5dbbe5f7a8e85595983e35b
amserhii11 <amelin.serhii@gmail.com>
2019-09-14 20:48:50 +0300

    Add search-bar

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,16 +3,17 @@ import './Main.scss';
 
 import Filters from '../Filters/Filters';
 import Cards from '../Cards/Cards';
+import SearchBar from '../SearchBar/SearchBar';
 
 // add button all events;
 
 const categoriesArray = [
-  "Фестивалі",
-  "Концерти",
-  "Вистави",
-  "Конференції",
-  "Вистави",
-  "Інше"
+  'Фестивалі',
+  'Концерти',
+  'Вистави',
+  'Конференції',
+  'Вистави',
+  'Інше',
 ];
 
 class Main extends React.Component {
@@ -25,11 +26,11 @@ class Main extends React.Component {
 
     return (
       <>
+        <SearchBar />
         <Filters categories={this.state.categories} />
         <Cards events={events} />
       </>
-
-    )
+    );
   }
 }
 
